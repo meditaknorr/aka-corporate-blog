b5dc7ea514a95bfebc65cb79c7dbe92aea5c9c7f
__MeditaKnorr <51007331+meditaknorr@users.noreply.github.com>
2023-09-29 10:02:24 +0200

    develop: Updated to laravel 10

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -3,14 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register(): void
     {
@@ -19,11 +16,9 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot(): void
     {
-      Schema::defaultStringLength(199);
+        //
     }
 }
